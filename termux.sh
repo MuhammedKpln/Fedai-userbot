2d3074797337a831f2a7a20b7b05b57077a7fdae
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-07-14 12:48:09 +0200

    chore: remove old scripts

--- termux.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install nodejs --fix-missing
-pkg install git
-git clone https://github.com/muhammedkpln/fedai-userbot
-cd fedai-userbot
-npm install @adiwajshing/baileys
-npm install chalk
-node qr.js
