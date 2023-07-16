d004ea7d395c184a206415bc040adcec49222b75
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2023-07-16 20:39:38 +0200

    v

--- src/plugins/voicy.ts
+++ src/plugins/voicy.ts
@@ -1,7 +1,7 @@
 import { downloadContentFromMessage } from "@whiskeysockets/baileys";
 import axios from "axios";
 import ffmpeg from "fluent-ffmpeg";
-// import { Wit, } from "node-wit";
+import { Wit, } from "node-wit";
 import { createReadStream } from "node:fs";
 import { writeFile } from "node:fs/promises";
 import { Readable } from "node:stream";
@@ -9,6 +9,9 @@ import { addCommand } from "../core/events";
 import { errorMessage, successfullMessage } from "../core/helpers";
 import { getString } from "../core/language";
 import { Logger } from "../core/logger";
+import  fetch from 'node-fetch'
+
+import { parse } from "node:path";
 const Lang = getString("voicy");
 
 function parseResponse(response) {
@@ -17,47 +20,116 @@ function parseResponse(response) {
     .map((x) => x.trim())
     .filter((x) => x.length > 0);
 
+    // console.log(chunks)
+
   let prev = "";
   let jsons = [];
   for (const chunk of chunks) {
     try {
       prev += chunk;
       //@ts-ignore
-      jsons.push(JSON.parse(prev));
+      jsons.push(JSON.parse(chunk));
       prev = "";
-    } catch (_e) {}
+    } catch (_e) {
+      // console.error(_e)
+    }
   }
 
   return jsons;
 }
 
+
+export const parseChunkedResponse = <Dto>(body = ""): Dto[] => {
+  // Split by newline, trim, remove empty lines
+  const chunks = body
+    .split("\r\n")
+    .map((chunk) => chunk.trim())
+    .filter((chunk) => Boolean(chunk.length));
+
+  // Loop through the chunks and try to Json.parse
+  return chunks.reduce<{ prev: string; acc: Dto[] }>(
+    ({ prev, acc }, chunk) => {
+      const newPrev = `${prev}${chunk}`;
+      try {
+        const newChunk: Dto = JSON.parse(newPrev);
+        return { prev: "", acc: [...acc, newChunk] };
+      } catch (err) {
+        return { prev: newPrev, acc };
+      }
+    },
+    { prev: "", acc: [] },
+  ).acc;
+};
+
 async function recognizeAudio(): Promise<string> {
   return new Promise(async (resolve) => {
     const headers = {
-      "Content-Type": "audio/wav",
+      "Content-Type": `audio/wav`,
       Authorization: `Bearer ${process.env.WITAI_API}`,
+      Accept: "application/json",
+
       "Transfer-Encoding": "chunked",
+
     };
 
     const file = createReadStream("./output.wav");
 
-    const response = await axios.post<Readable>(
-      "https://api.wit.ai/dictation?v=20230215",
-      file,
-      {
-        headers: headers,
-        responseType: "stream",
-      },
-    );
 
-    response.data.on("readable", () => {
-      let chunk;
-      let contents = "";
-      while (null !== (chunk = response.data.read())) {
-        contents += chunk.toString();
+
+
+
+
+
+    const response = await axios.request<string>(
+      {
+        method: "POST",
+        url: "https://api.wit.ai/dictation",
+        params: {
+          v: 20230215,
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.WITAI_API}`,
+          Accept: "application/json",
+          "Content-Type": `audio/wav`,
+          "Transfer-Encoding": "chunked",
+        },
+        timeout: 10_000,
+        maxContentLength: Infinity,
+        maxBodyLength: Infinity,
+        responseType: "text",
+        data: file,
+        transformResponse: (d) => d,
+      }
+    ).then((response) => {
+      const chunks = parseChunkedResponse(response.data);
+      const finalizedChunks = chunks.filter(
+        ({ is_final: isFinal }) => isFinal,
+      );
+      if (!finalizedChunks.length) {
+        Logger.warn(
+          `The final response chunk not found. Transcription is empty.`,
+          chunks.map(({ text }) => text),
+        );
       }
 
-      console.log(JSON.parse());
+      console.log(finalizedChunks)
+      return finalizedChunks;
+    })
+
+
+
+
+    // // wit.dictation('audio/wav', response.body)
+    // response.data.on("readable", () => {
+    //   let chunk;
+    //   let contents = "";
+    //   while (null !== (chunk = response.body.read())) {
+    //     contents += chunk.toString()
+    //   }
+
+    //   console.log(parseResponse(contents))
+
+      // console.log(JSON.parse());
 
       // for (const rsp of parseResponse(contents)) {
       //   const { error, intents, is_final, text } = rsp;
@@ -75,7 +147,7 @@ async function recognizeAudio(): Promise<string> {
     //   await rm("output.ogg");
     //   await rm("output.wav");
     // });
-  });
+  // });
 }
 const convertToWav = (file) => {
   return ffmpeg(file)
@@ -117,18 +189,14 @@ addCommand(
               await writeFile("output.ogg", chunks);
 
               convertToWav("output.ogg").on("end", async () => {
-                // const wit = new Wit({
-                //   accessToken: process.env.WITAI_API!,
-                // });
 
-                // wit.
                 const recognizedText = await recognizeAudio();
 
-                await message.edit(
-                  successfullMessage(
-                    Lang["TEXT"] + "```" + recognizedText + "```",
-                  ),
-                );
+                // await message.edit(
+                //   successfullMessage(
+                //     Lang["TEXT"] + "```" + recognizedText + "```",
+                //   ),
+                // );
               });
             });
 
